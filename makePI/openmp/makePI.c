82e3da7d5f824e2849b12dae8d5585537a97dcde
Hannes Badertscher <h_badertscher@bluewin.ch>
2014-03-24 13:14:17 +0100

    Try with OpenMP - not yet finished!

---
+++ makePI/openmp/makePI.c
@@ -0,0 +1,81 @@
+/* ********************************************************** *
+ * makePI.c                                                   *
+ * 24.03.2014 / HBA                                           */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include <float.h>
+#include <pthread.h>
+
+
+#define NUMOFITERATIONS 10000
+ 
+// Function Prototypes
+double getRandom(double min, double max);
+int isInCircle(double x, double y);
+ 
+/* ********************************************************** *
+ * main                                                       */
+int main(int argc, const char * argv[]) {
+  int i;
+  double x,y;
+  int sum;
+  double numOfIterations;
+  
+  if(argc > 1)
+    sscanf(argv[1],"%lf",&numOfIterations);
+  else
+    numOfIterations = NUMOFITERATIONS;
+ 
+  srand(time(NULL));
+  
+#pragma omp parallel for
+  for(i=0;i<numOfIterations;i++) {
+    x = getRandom(0,1);
+    y = getRandom(0,1);
+//   printf("(%f,%f)\n",x,y);
+    if(isInCircle(x,y)) {
+      sum++;
+    }
+  }
+  
+  printf("Sum: %d \n", sum);
+  
+  printf("PI is exactly %f\n", 4*((double)sum)/((double)numOfIterations));
+  
+  return 0;
+ }
+
+/* ********************************************************** *
+ * Create a Random Number between min and max                 */
+double getRandom(double min, double max)
+{
+  return (((double)rand())/RAND_MAX)*(max-min)+min;
+  
+//   FILE* devRandom;
+//   unsigned char randomChar;
+//   double randomDouble;
+//   int i;
+//   
+//   devRandom = fopen("/dev/urandom", "r");
+//   fscanf(devRandom,"%c",&randomInt);
+//   fclose(devRandom);
+//   
+//   return randomDouble;
+}
+
+/* ********************************************************** *
+ * Checks if a point (x,y) is inside the unit circle          */
+int isInCircle(double x, double y)
+{
+  double res;
+  
+  res = x*x + y*y;
+  
+  if(res <= 1)
+    return 1;
+  else
+    return 0;
+}
